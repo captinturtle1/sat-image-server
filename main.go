3b50844545d3850ef8dce6efec5b6497b601f0b6
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2025-10-05 18:27:46 -0600

    added CORS

--- main.go
+++ main.go
@@ -21,6 +21,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/nfnt/resize"
 )
@@ -72,6 +73,15 @@ func main() {
 	}
 
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		// AllowOrigins:     []string{"*"}, // For Development
+		AllowOrigins:     []string{"https://mission.austinlopez.work"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
 
 	router.GET("/ping", ping)
 	router.GET("/missions", api.getMissions)
